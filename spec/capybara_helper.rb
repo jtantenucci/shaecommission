c639a26f3aec605d2afe063f81efb6eabaafb716
Jake Antenucci <jtantenucci@gmail.com>
2024-07-16 14:51:10 -0400

    try new host url

--- spec/capybara_helper.rb
+++ spec/capybara_helper.rb
@@ -2,7 +2,7 @@ require 'capybara/session'
 require_relative 'selenium_helper'
 
 Capybara.default_driver = :selenium_chrome_headless
-Capybara.app_host = 'shaecommission.vercel.app'
+Capybara.app_host = 'https://www.shaecommission.vercel.app'
 Capybara.run_server = false
 Capybara.default_max_wait_time = 15
 Capybara.javascript_driver = ENV.fetch('CAPYBARA_JAVASCRIPT_DRIVER', :selenium_chrome_headless).to_sym
